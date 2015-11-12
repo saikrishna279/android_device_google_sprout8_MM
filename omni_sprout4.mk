8a6861260a82cb0248d6983b7df77ea218a1cfa0
Anik khan <anik9280@gmail.com>
2015-11-12 14:45:08 +0600

    sprout4 : Update Fingerprint from M
    Signed-off-by: Anik khan <anik9280@gmail.com>

--- omni_sprout4.mk
+++ omni_sprout4.mk
@@ -6,7 +6,7 @@ $(call inherit-product, vendor/omni/config/common.mk)
 # Pull GSM stuff
 $(call inherit-product, vendor/omni/config/gsm.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=5.1/LMY47O/1783956:user/release-keys PRIVATE_BUILD_DESC="sprout-user 5.1 LMY47O 1783956 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=6.0/MRA58M/2280749:user/release-keys PRIVATE_BUILD_DESC="sprout-user 6.0 MRA58M 2280749 release-keys"
 
 PRODUCT_NAME := omni_sprout4
 PRODUCT_DEVICE :=sprout4
