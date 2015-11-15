85cbe296ed1093bb271127316306696ee1e931b5
Anik khan <anik9280@gmail.com>
2015-11-15 14:59:00 +0600

    sprout4: Lets build Tesla
    Signed-off-by: Anik khan <anik9280@gmail.com>

---
+++ optimization.mk
@@ -0,0 +1,34 @@
+export USE_HOST_LEX := yes
+export USE_CCACHE := 1
+export USE_SYSTEM_CCACHE := 1
+export USE_ORACLE_JAVA := 1
+
+# Build
+export ANDROID_COMPILE_WITH_JACK := true
+export DISABLE_OPTIMIZATIONS := true
+ifneq ($(DISABLE_OPTIMIZATIONS),true)
+export DONT_ERROROUT := true
+export USE_O3_OPTIMIZATIONS := true
+export FORCE_DISABLE_DEBUGGING := true
+export ENABLE_GOLD_LINKER := true
+export ENABLE_IPA_ANALYSER := true
+export TARGET_USE_PIPE := true
+export KRAIT_TUNINGS := true
+export ENABLE_PTHREAD := true
+export ENABLE_GOMP := true
+export GRAPHITE_OPTS := true
+export ENABLE_EXTRAGCC := true
+export STRICT_ALIASING := true
+export ENABLE_SANITIZE := true
+endif 
+
+# Toolchain and other
+export TARGET_NDK_GCC_VERSION := 4.8
+export TARGET_GCC_VERSION_EXP := 4.8
+export TARGET_GCC_VERSION := 4.8
+export TARGET_GCC_VERSION_KERNEL := 4.9
+
+# Stuff
+export KBUILD_BUILD_USER := Anik
+export KBUILD_BUILD_HOST := Phoenix
+export TARGET_DEVICE := sprout
--- tesla.mk
+++ tesla.mk
@@ -1,11 +1,14 @@
 $(call inherit-product, device/google/sprout4/sprout4.mk)
 
-# Common CM stuff
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Common Tesla stuff
+$(call inherit-product, vendor/tesla/config/common_full_phone.mk)
+
+# Optimizations
+$(call inherit-product, device/google/sprout4/optimization.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=6.0/MRA58M/2280749:user/release-keys PRIVATE_BUILD_DESC="sprout-user 6.0 MRA58M 2280749 release-keys"
 
-PRODUCT_NAME := cm_sprout4
+PRODUCT_NAME := tesla_sprout4
 PRODUCT_DEVICE :=sprout4
 PRODUCT_BRAND := google
 PRODUCT_MANUFACTURER := Google
